bf4af452562321328f1ec7c3a4031a06d350790e
RafalKazubowski <rkazubowski@gmail.com>
2020-05-14 19:23:22 +0200

    scraper modification

--- scraping flats and houses/scraper_gratkapl.py
+++ scraping flats and houses/scraper_gratkapl.py
@@ -35,14 +35,22 @@ class PageScrapper:
         container = page.find('div', {'class': 'sticker__container column small-12'}) 
 
         data['tytul'] = container.find('h1', {'class': 'sticker__title'}).get_text().strip()
-        data['cena'] = int(container.find('div', {'class': 'priceInfo'}).find('span',{'class':"priceInfo__value"}).get_text().strip().replace('zł', '').replace(' ', '').replace(',','.'))
+        data['cena'] = float(container.find('div', {'class': 'priceInfo'}).find('span',{'class':"priceInfo__value"}).get_text().strip().replace('zł', '').replace(' ', '').replace(',','.'))
         data['cena_waluta'] = container.find('div', {'class': 'priceInfo'}).find('span',{'class':"priceInfo__currency"}).get_text().strip()
-        data['cena_za_metr'] = float(container.find('div', {'class': 'priceInfo'}).find('span',{'class':"priceInfo__additional"}).get_text().strip().replace('zł/m2', '').replace(' ', '').replace(',','.'))
+        
+        if container.find('div', {'class': 'priceInfo'}).find('span',{'class':"priceInfo__additional"}) is not None:
+            data['cena_za_metr'] = float(container.find('div', {'class': 'priceInfo'}).find('span',{'class':"priceInfo__additional"}).get_text().strip().replace('zł/m2', '').replace(' ', '').replace(',','.'))
+        else:
+            data['cena_za_metr'] = float('nan')
 
         container2 = page.find('div', {'class': 'offer__inner row'}) 
         
         data['id_ogloszenia'] = container2.find('p', {'class': 'contact__offerId'}).find('span').get_text().strip()
-        data['tresc'] = container2.find('div', {'class': 'description'}).find('div', {'class': 'description__rolled ql-container'}).get_text().strip()
+
+        if container2.find('div', {'class': 'description'}) is not None:
+            data['tresc'] = container2.find('div', {'class': 'description'}).find('div', {'class': 'description__rolled ql-container'}).get_text().strip()
+        else:
+            data['tresc'] = float('nan')
 
         if container2.find('div', {'class': 'gallery'}) is not None:
             data['zdjecie'] = True
@@ -62,53 +70,54 @@ class PageScrapper:
                 if parameter_value is not None:
                     data[slugify(parameter_title.get_text())] = parameter_value.get_text()
                 
-                if (parameters[item].find('span').get_text() == "Dostępność od"):
-                    data['data_dostepne'] = parameters[item].find('b',{'class':'parameters__value'}).get_text().strip()
-                    #print(data['data_dostepne'])
+                if parameters[item].find('span') is not None:
+                    if (parameters[item].find('span').get_text() == "Dostępność od"):
+                        data['data_dostepne'] = parameters[item].find('b',{'class':'parameters__value'}).get_text().strip()
+                        #print(data['data_dostepne'])
 
-                if (parameters[item].find('span').get_text() == "Powierzchnia w m2"):
-                    data['powierzchnia'] = float(parameters[item].find('b',{'class':'parameters__value'}).get_text().strip().replace('m2', '').replace(' ', '').replace(',','.'))
-                    #print(data['powierzchnia'])
+                    if (parameters[item].find('span').get_text() == "Powierzchnia w m2"):
+                        data['powierzchnia'] = float(parameters[item].find('b',{'class':'parameters__value'}).get_text().strip().replace('m2', '').replace(' ', '').replace(',','.'))
+                        #print(data['powierzchnia'])
 
-                if (parameters[item].find('span').get_text() == "Liczba pokoi"):
-                    data['liczba_pokoi'] = parameters[item].find('b',{'class':'parameters__value'}).get_text().strip()
-                    #print(data['liczba_pokoi'])
+                    if (parameters[item].find('span').get_text() == "Liczba pokoi"):
+                        data['liczba_pokoi'] = parameters[item].find('b',{'class':'parameters__value'}).get_text().strip()
+                        #print(data['liczba_pokoi'])
 
-                if (parameters[item].find('span').get_text() == "Liczba pięter w budynku"):
-                    data['liczba_pieter_budynku'] = parameters[item].find('b',{'class':'parameters__value'}).get_text().strip()
-                    #print(data['liczba_pieter_budynku'])
+                    if (parameters[item].find('span').get_text() == "Liczba pięter w budynku"):
+                        data['liczba_pieter_budynku'] = parameters[item].find('b',{'class':'parameters__value'}).get_text().strip()
+                        #print(data['liczba_pieter_budynku'])
 
-                if (parameters[item].find('span').get_text() == "Typ zabudowy"):
-                    data['rodzaj_zabudowy'] = parameters[item].find('b',{'class':'parameters__value'}).get_text().strip()
-                    #print(data['rodzaj_zabudowy'])
-                
-                if (parameters[item].find('span').get_text() == "Rok budowy"):
-                    data['rok_budowy'] = int(parameters[item].find('b',{'class':'parameters__value'}).get_text().strip())
-                    #print(data['rok_budowy'])
+                    if (parameters[item].find('span').get_text() == "Typ zabudowy"):
+                        data['rodzaj_zabudowy'] = parameters[item].find('b',{'class':'parameters__value'}).get_text().strip()
+                        #print(data['rodzaj_zabudowy'])
+                    
+                    if (parameters[item].find('span').get_text() == "Rok budowy"):
+                        data['rok_budowy'] = int(parameters[item].find('b',{'class':'parameters__value'}).get_text().strip())
+                        #print(data['rok_budowy'])
 
-                if (parameters[item].find('span').get_text() == "Lokalizacja"):
-                    data['adres'] = parameters[item].find('b',{'class':'parameters__value'}).get_text().strip().replace(' ', '')
-                    #print(data['adres'])
+                    if (parameters[item].find('span').get_text() == "Lokalizacja"):
+                        data['adres'] = parameters[item].find('b',{'class':'parameters__value'}).get_text().strip().replace(' ', '')
+                        #print(data['adres'])
 
-                if (parameters[item].find('span').get_text() == "Piętro"):
-                    data['pietro'] = parameters[item].find('b',{'class':'parameters__value'}).get_text().strip()
-                    #print(data['pietro'])
+                    if (parameters[item].find('span').get_text() == "Piętro"):
+                        data['pietro'] = parameters[item].find('b',{'class':'parameters__value'}).get_text().strip()
+                        #print(data['pietro'])
 
-                if (parameters[item].find('span').get_text() == "Typ zabudowy"):
-                    data['typ_domu'] = parameters[item].find('b',{'class':'parameters__value'}).get_text().strip()
-                    #print(data['typ_domu'])
+                    if (parameters[item].find('span').get_text() == "Typ zabudowy"):
+                        data['typ_domu'] = parameters[item].find('b',{'class':'parameters__value'}).get_text().strip()
+                        #print(data['typ_domu'])
 
-                if (parameters[item].find('span').get_text() == "Powierzchnia działki w m2"):
-                    data['powierzchnia_dzialki'] = float(parameters[item].find('b',{'class':'parameters__value'}).get_text().strip().replace('m2', '').replace(' ', '').replace(',','.'))
-                    #print(data['powierzchnia_dzialki'])
+                    if (parameters[item].find('span').get_text() == "Powierzchnia działki w m2"):
+                        data['powierzchnia_dzialki'] = float(parameters[item].find('b',{'class':'parameters__value'}).get_text().strip().replace('m2', '').replace(' ', '').replace(',','.'))
+                        #print(data['powierzchnia_dzialki'])
 
-                if (parameters[item].find('span').get_text() == "Stan"):
-                    data['wykonczenie'] = parameters[item].find('b',{'class':'parameters__value'}).get_text().strip()
-                    #print(data['wykonczenie'])
+                    if (parameters[item].find('span').get_text() == "Stan"):
+                        data['wykonczenie'] = parameters[item].find('b',{'class':'parameters__value'}).get_text().strip()
+                        #print(data['wykonczenie'])
 
-                if (parameters[item].find('span').get_text() == "Forma kuchni"):
-                    data['kuchnia'] = parameters[item].find('b',{'class':'parameters__value'}).get_text().strip()
-                    #print(data['kuchnia'])
+                    if (parameters[item].find('span').get_text() == "Forma kuchni"):
+                        data['kuchnia'] = parameters[item].find('b',{'class':'parameters__value'}).get_text().strip()
+                        #print(data['kuchnia'])
 
         return data
     
@@ -135,18 +144,18 @@ class PageScrapper:
                     
 
     def save_json(self):
-        with open('gratkapl_mieszkania_sopot.json', 'w') as file:
+        with open('gratkapl_mieszkania_gdansk.json', 'w') as file:
             json.dump(self.find_advertisements(), file)
 
 
 
-URL1 = 'https://gratka.pl/nieruchomosci/mieszkania/gdansk/sprzedaz'
-URL2 = 'https://gratka.pl/nieruchomosci/mieszkania/gdynia/sprzedaz'
+URL1 = 'https://gratka.pl/nieruchomosci/mieszkania/gdansk/sprzedaz?page=1&cena-calkowita:min=100000'
+URL2 = 'https://gratka.pl/nieruchomosci/mieszkania/gdynia/sprzedaz?page=1&cena-calkowita:min=100000'
 URL3 = 'https://gratka.pl/nieruchomosci/mieszkania/sopot/sprzedaz?page=1&cena-calkowita:min=100000'
-URL4 = 'https://gratka.pl/nieruchomosci/domy/gdansk/sprzedaz'
-URL5 = 'https://gratka.pl/nieruchomosci/domy/gdynia/sprzedaz'
+URL4 = 'https://gratka.pl/nieruchomosci/domy/gdansk/sprzedaz?page=1&cena-calkowita:min=100000'
+URL5 = 'https://gratka.pl/nieruchomosci/domy/gdynia/sprzedaz?page=1&cena-calkowita:min=100000'
 URL6 = 'https://gratka.pl/nieruchomosci/domy/sopot/sprzedaz?page=1&cena-calkowita:min=100000'
-scraper = PageScrapper(URL3)
+scraper = PageScrapper(URL1)
 #scraper.find_advertisements()
 scraper.save_json()
 
